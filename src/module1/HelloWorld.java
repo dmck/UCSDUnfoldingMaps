c36cc79eae436cfb1a094ad3200f1407354f1115
dmck <dmck@users.noreply.github.com>
2016-02-01 11:47:49 -0500

    Add second map

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -11,8 +11,8 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 /** HelloWorld
   * An application with two maps side-by-side zoomed in on different locations.
   * Author: UC San Diego Coursera Intermediate Programming team
-  * @author Your name here
-  * Date: July 17, 2015
+  * @author Daniel McKnight
+  * Date: February 1, 2016
   * */
 public class HelloWorld extends PApplet
 {
@@ -36,7 +36,7 @@ public class HelloWorld extends PApplet
 	UnfoldingMap map2;
 
 	public void setup() {
-		size(800, 600, P2D);  // Set up the Applet window to be 800x600
+		size(850, 600, P2D);  // Set up the Applet window to be 850x600
 		                      // The OPENGL argument indicates to use the 
 		                      // Processing library's 2D drawing
 		                      // You'll learn more about processing in Module 3
@@ -74,8 +74,15 @@ public class HelloWorld extends PApplet
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
 		
-		// TODO: Add code here that creates map2 
-		// Then you'll modify draw() below
+		// Map2
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+
+		// The next line zooms in and centers the map at 
+	    // 32.9 (latitude) and -117.2 (longitude)
+	    map2.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
+		
+		// This line makes the map interactive
+		MapUtils.createDefaultEventDispatcher(this, map2);
 
 	}
 
@@ -84,6 +91,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
