16b7e308bd824ee4b7eacb156fcc78440cf4ba37
dmck <dmck@users.noreply.github.com>
2016-02-01 12:02:22 -0500

    Change default location of map2

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -79,7 +79,7 @@ public class HelloWorld extends PApplet
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
-	    map2.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
+	    map2.zoomAndPanTo(zoomLevel, new Location(27.9f,-82.4f));
 		
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map2);
